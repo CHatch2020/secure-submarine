a382e06dea01049da73689b9d08a1ddb5487b09d
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-15 16:19:55 -0600

    revamped sql

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -8,21 +8,11 @@ const {
 router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-  let sqlText;
-  let sqlValue;
-  if (req.user.clearance_level >= 13) {
-    sqlText = `SELECT * FROM "secret";`;
-    sqlValue = []
-  } else  if (req.user.clearance_level >= 6 & req.user.clearance_level < 13) {
-    sqlText = `SELECT * FROM "secret"
-    WHERE "secrecy_level" < $1;
-    `;
-    sqlValue = [13];
-  } else {
-    sqlText = `SELECT * FROM "secret"
-    WHERE "secrecy_level" < $1;`;
-    sqlValue = [6];
-  }
+  let sqlText = `
+  SELECT * FROM "secret"
+  WHERE "secrecy_level" < $1;
+  `;
+  let sqlValue = [req.user.clearance_level];
 
   pool
     .query(sqlText, sqlValue)
